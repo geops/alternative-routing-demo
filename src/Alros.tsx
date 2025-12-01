1b80cc29810fdabc75a438fbe19863778c7a9c90
Olivier Terral <olivier.terral@geops.com>
2025-12-01 11:56:09 +0100

    fix: remove hover effect

--- src/Alros.tsx
+++ src/Alros.tsx
@@ -3,7 +3,10 @@ import useAlroContext from "./hooks/useAlroContext";
 import { AnnotatedAlternativeRoutes } from "./types";
 
 function Alros() {
-  const { alros, selectedAlro } = useAlroContext();
+  const {
+    alros,
+    // selectedAlro
+  } = useAlroContext();
 
   console.log("Rerendering Alros with alros:", alros);
   return (
@@ -13,8 +16,10 @@ function Alros() {
           <Alro
             alro={alro}
             className={
-              "rounded border p-4 text-left hover:border-blue-500 hover:border-2 cursor-pointer " +
-              (alro === selectedAlro ? " bg-gray-200" : " bg-white")
+              "rounded border p-4 text-left "
+
+              // " hover:border-blue-500 hover:border-2 cursor-pointer " +
+              // (alro === selectedAlro ? " bg-gray-200" : " bg-white")
             }
             key={JSON.stringify(alro)}
           ></Alro>
