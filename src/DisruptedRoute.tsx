54257252087f4bfadac883476daba891105b3211
Olivier Terral <olivier.terral@geops.com>
2025-12-03 11:57:19 +0100

    fix: Display more infos

--- src/DisruptedRoute.tsx
+++ src/DisruptedRoute.tsx
@@ -7,8 +7,10 @@ function DisruptedRoute(props: JSX.IntrinsicElements["div"]) {
   const { selectedExample } = useAlroContext();
   let timeIntervalsText;
   const addInfo = selectedExample?.additionalInfo;
-  // @ts-expect-error - bad type defintions
-  const label = addInfo?.requested_stops.join(" → ");
+  const label = [
+    addInfo?.requested_stops[0],
+    addInfo?.requested_stops[addInfo?.requested_stops.length - 1],
+  ].join(" → ");
 
   const timeIntervals = // @ts-expect-error - we know
     addInfo.disruption_scenario.lineDisruptions[0].timeIntervals[0];
@@ -35,13 +37,38 @@ function DisruptedRoute(props: JSX.IntrinsicElements["div"]) {
       },
     );
 
-  const lines = [...new Set(linesNames)]; // unique
+  const lines = [...new Set(linesNames)].sort(); // unique
   const affectedLinesText = `Affected lines: ${lines.join(", ")}`;
+
+  // @ts-expect-error - we know
+  const stopsNames = addInfo?.disruption_scenario.lineDisruptions.flatMap(
+    // @ts-expect-error - we know
+    (lineDisruption) => {
+      return lineDisruption.disruptedLines.flatMap(
+        // @ts-expect-error - we know
+        (disruptedLine) => {
+          const sections = disruptedLine.sections;
+          // @ts-expect-error - we know
+          return sections.flatMap((section) => {
+            return [section.fromEvaNumber, section.toEvaNumber];
+          });
+        },
+      );
+    },
+  );
+
+  const stops = [...new Set(stopsNames)].sort(); // unique
+  const affectedStopsText = `Affected stops: ${stops.join(", ")}`;
+
+  // @ts-expect-error - we know
+  const requestedStopsText = `Requested stops: ${(addInfo?.requested_stops || []).join(", ")}`;
   return (
     <div {...props}>
       <div className="overflow-hidden text-ellipsis font-bold">{label}</div>
       <div className="text-xs">{timeIntervalsText}</div>
+      <div className="text-xs">{requestedStopsText}</div>
       <div className="text-xs">{affectedLinesText}</div>
+      <div className="text-xs">{affectedStopsText}</div>
     </div>
   );
 }
